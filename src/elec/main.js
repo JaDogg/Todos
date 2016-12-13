616caa35cc5e01924eb97b4d99cb7211019ae0fc
Bhathiya Perera <jadogg.coder@gmail.com>
2016-12-13 18:29:34 +0530

    Added icon, fixed close issue
    Signed-off-by: Bhathiya Perera <jadogg.coder@gmail.com>

--- src/elec/main.js
+++ src/elec/main.js
@@ -3,20 +3,21 @@ var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
 var ipc = electron.ipcMain;
 
-app.on('ready', function () {
-    var appWindow;
-    appWindow = new BrowserWindow({
-        show: false,
-        width: 400,
-        height: 500,
-        maximizable: false
-    });
-    appWindow.loadURL('file://' + __dirname + '/app.html');
 
+let appWindow;
+app.on('ready', function () {
+
+    appWindow = new BrowserWindow({show: false, width: 400, height: 500, maximizable: false});
+    appWindow.loadURL('file://' + __dirname + '/app.html');
 
     appWindow.once('ready-to-show', function () {
         appWindow.show();
     });
-    //appWindow.setMenu(null);
-    
+    appWindow.setMenu(null);
+});
+
+app.on('window-all-closed', app.quit);
+app.on('before-quit', () => {
+    appWindow.removeAllListeners('close');
+    appWindow.close();
 });
